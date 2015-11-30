d1ab0ab3151eba20e5a20a63e1e717e8e8c4f815
thecakeisalie25 <larsona@outlook.com>
2015-11-30 12:59:10 -0600

    Added donnie and some more commits
    need to fix the for loop though, maybe donnie will help

--- Inputoutput.js
+++ Inputoutput.js
@@ -1,11 +1,22 @@
 var name = prompt("What is your name, sonny?");
 if (name === "Larson"){
-console.log("Hello Larson!");
+  console.log("Hello Larson!");
 }
 else if(name === "Grant"){
   console.log("Hello Grant!")
 }
+else if (name === "Donnie"){
+  console.log("Hello Donnie!")
+}
 else{
   console.log("I don't know you, sonny! GET OfF MY LAWN")
-  confirm("Get off his lawn?");
+  if (confirm("Get off his lawn?");){
+      console.log("Running as fast as you can!")
+      confirm("Take a break?");
+      console.log("You are so tired it doesn't matter if you want to.")
+      for(var i=0;i>5;i++){
+          confirm("Resting...");
+      }
+     
+  }
 }
