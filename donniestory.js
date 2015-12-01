92e2509c39d541efe8e81a4a7049b6ee7468fedf
thecakeisalie25 <larsona@outlook.com>
2015-12-01 14:55:16 -0600

    MAJOR change to naming engine.
    Merge this to donnie-text-story if you want, just something I thought I'd try.

--- donniestory.js
+++ donniestory.js
@@ -1,7 +1,2 @@
 var name = prompt("(A man approaches you as you walk in a large building. The sign said 'Nintendo Inc.') Hello, young child... What is your name?");
-if (name === "Donnie"){
-  console.log("Well, hello there, Donnie! Nice to meet you. (The man reaches out to shake your hand.) ");
-}
-else if (name === Lars){
-  console.log("Well, hi there, Lars! Nice to meet you. ")
-}
+console.log("Well, hello there, "+name+"! Nice to meet you. (The man reaches out to shake your hand.) ")
