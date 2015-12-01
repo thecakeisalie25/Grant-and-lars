f1ae9901466a3e629b38d0f5733cb58ebf1afd0c
Donnie Hudson <guitarheroexpert234@live.com>
2015-12-01 12:57:52 -0600

    Update donniestory.js

--- donniestory.js
+++ donniestory.js
@@ -1,1 +1,7 @@
-var name = 
+var name = prompt("(A man approaches you as you walk in a large building. The sign said 'Nintendo Inc.') Hello, young child... What is your name?");
+if (name === "Donnie"){
+  console.log("Well, hello there, Donnie! Nice to meet you. (The man reaches out to shake your hand.) ");
+}
+else if (name === Lars){
+  console.log("Well, hi there, Lars! Nice to meet you. ")
+}
